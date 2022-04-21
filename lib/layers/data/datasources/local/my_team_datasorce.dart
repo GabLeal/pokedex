ffce975461030621a1fb2cd89c59a953ecb9cdc1
GabLeal <gabriel.leal@compasso.com.br>
2022-04-21 16:43:23 -0300

    movendo arquivos

--- lib/layers/data/datasources/local/my_team_datasorce.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pokedex/layers/domain/entities/pokemon_entity.dart';
-
-abstract class MyTeamDatasource {
-  Future<bool> add(PokemonEntity pokemon);
-  Future<List<PokemonEntity>> fetch();
-  Future<bool> remove(int idPokemon);
-}
