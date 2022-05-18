49b0703f780a0e4f7b0b2ea58ce31a747ff89e07
GabLeal <gabriel.leal@compasso.com.br>
2022-05-18 17:47:06 -0300

    refatoraçao nos testes na parte de injetar as dependencias

--- test/stores/pokemon_store_test.dart
+++ test/stores/pokemon_store_test.dart
@@ -1,64 +1,37 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:pokedex/cache/cache_favorites.dart';
 import 'package:pokedex/layers/data/dto/pokemon_dto.dart';
-import 'package:pokedex/layers/domain/repositories/pokemon_repository.dart';
+import 'package:pokedex/layers/domain/usercases/my_favorites/my_favorites_use_case.dart';
+import 'package:pokedex/layers/domain/usercases/my_team/my_team_use_case.dart';
 import 'package:pokedex/layers/domain/usercases/pokemon/pokemon_use_case.dart';
-import 'package:pokedex/layers/domain/usercases/pokemon/pokemon_use_case_imp.dart';
 import 'package:pokedex/layers/presentation/stores/pokemon_store.dart';
 
-class PokemonRepositoryMock extends Mock implements PokemonRepository {}
+class PokemonUseCaseMock extends Mock implements PokemonUseCase {}
 
-class CacheFavoritesMock extends Mock implements CacheFavorites {}
+class MyTeamUseCaseMock extends Mock implements MyTeamUseCase {}
+
+class MyFavoritesUseCaseMock extends Mock implements MyFavoritesUseCase {}
 
 main() {
-  final cacheFavorites = CacheFavoritesMock();
+  final pokemonUseCase = PokemonUseCaseMock();
+  final myTeamUseCase = MyTeamUseCaseMock();
+  final myFavoritesUseCase = MyFavoritesUseCaseMock();
 
-  GetIt getIt = GetIt.instance;
-
-  getIt.registerLazySingleton<PokemonRepository>(
-    () => PokemonRepositoryMock(),
+  final store = PokemonStore(
+    pokemonUseCase,
+    myTeamUseCase,
+    myFavoritesUseCase,
   );
-
-  getIt.registerLazySingleton<PokemonUseCase>(
-    () => PokemonUseCaseImp(
-      getIt.get<PokemonRepository>(),
-    ),
-  );
-
-  getIt.registerLazySingleton<PokemonStore>(
-    () => PokemonStore(
-      getIt.get<PokemonUseCase>(),
-      cacheFavorites,
-    ),
-  );
-
-  final store = getIt.get<PokemonStore>();
-  //PokemonStore(repository, cacheFavorites);
-
-  group('Cache favorites pokemons', () {
-    test('deve adicionar um pokemon aos favoritos', () {
-      PokemonDto pokemon = PokemonDto();
-      when(() => cacheFavorites.favoritePokemon(pokemon))
-          .thenAnswer((_) async => Future.value(true));
-
-      store.favoritePokemon(pokemon);
-
-      expect(store.favoritesPokemons, isEmpty);
-    });
-  });
 
   group('API Pokemons', () {
     test('should return an empty pokemon list', () async {
-      when(() => getIt.get<PokemonRepository>().getPokemons())
-          .thenAnswer((_) async => []);
+      when(() => pokemonUseCase.getPokemons()).thenAnswer((_) async => []);
       await store.getPokemons();
 
       expect(store.pokemons, isEmpty);
     });
     test('should fill Pokemon list', () async {
-      when(() => getIt.get<PokemonRepository>().getPokemons())
+      when(() => pokemonUseCase.getPokemons())
           .thenAnswer((_) async => <PokemonDto>[
                 PokemonDto(),
                 PokemonDto(),
