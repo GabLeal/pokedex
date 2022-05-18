7444d4c29821067d68df3f0d3afb40b5384167ee
GabLeal <gabriel.leal@compasso.com.br>
2022-05-18 17:47:19 -0300

    injetando dependencias

--- lib/core/inject/inject.dart
+++ lib/core/inject/inject.dart
@@ -1,15 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:pokedex/cache/cache_favorites.dart';
+import 'package:pokedex/layers/data/datasources/local/my_favorities/my_favorites_datasorce.dart';
+import 'package:pokedex/layers/data/datasources/local/my_favorities/my_favorites_datasorce_imp.dart';
 import 'package:pokedex/layers/data/datasources/local/my_team/my_team_datasorce.dart';
 import 'package:pokedex/layers/data/datasources/local/my_team/my_team_datasource_imp.dart';
 import 'package:pokedex/layers/data/repositories/ability_repository_imp.dart';
 import 'package:pokedex/layers/data/repositories/move_repository_imp.dart';
+import 'package:pokedex/layers/data/repositories/my_favorites_repository_imp.dart';
 import 'package:pokedex/layers/data/repositories/my_team_repository_imp.dart';
 import 'package:pokedex/layers/data/repositories/pokemon_repository_imp.dart';
 import 'package:pokedex/layers/data/repositories/type_damage_repository_imp.dart';
 import 'package:pokedex/layers/domain/repositories/ability_repository.dart';
 import 'package:pokedex/layers/domain/repositories/move_repository.dart';
+import 'package:pokedex/layers/domain/repositories/my_favorites_repository.dart';
 import 'package:pokedex/layers/domain/repositories/my_team_repository.dart';
 import 'package:pokedex/layers/domain/repositories/pokemon_repository.dart';
 import 'package:pokedex/layers/domain/repositories/type_damage_repository.dart';
@@ -17,6 +20,8 @@ import 'package:pokedex/layers/domain/usercases/ability/ability_details_use_case
 import 'package:pokedex/layers/domain/usercases/ability/ability_details_use_case_imp.dart';
 import 'package:pokedex/layers/domain/usercases/moves/moves_use_case.dart';
 import 'package:pokedex/layers/domain/usercases/moves/moves_use_case_imp.dart';
+import 'package:pokedex/layers/domain/usercases/my_favorites/my_favorites_use_case.dart';
+import 'package:pokedex/layers/domain/usercases/my_favorites/my_favorites_use_case_imp.dart';
 import 'package:pokedex/layers/domain/usercases/my_team/my_team_use_case.dart';
 import 'package:pokedex/layers/domain/usercases/my_team/my_team_use_case_imp.dart';
 import 'package:pokedex/layers/domain/usercases/pokemon/pokemon_use_case.dart';
@@ -35,6 +40,9 @@ class Inject {
     //TODO datasourcer
     getIt.registerLazySingleton<MyTeamDatasource>(
       () => MyTeamDatasourceImp(),
+    );
+    getIt.registerLazySingleton<MyFavoritesDatasource>(
+      () => MyFavoritesDatasourceImp(),
     );
     //REPOSITORIES
 
@@ -65,6 +73,12 @@ class Inject {
     getIt.registerLazySingleton<MyTeamRepository>(
       () => MyTeamRepositoryImp(
         getIt<MyTeamDatasource>(),
+      ),
+    );
+
+    getIt.registerLazySingleton<MyFavoritesRepository>(
+      () => MyFavoritesRepositoryImp(
+        getIt<MyFavoritesDatasource>(),
       ),
     );
 
@@ -99,6 +113,12 @@ class Inject {
       ),
     );
 
+    getIt.registerLazySingleton<MyFavoritesUseCase>(
+      () => MyFavoritesUseCaseImp(
+        getIt<MyFavoritesRepository>(),
+      ),
+    );
+
     //CONTROLLERSß
     getIt.registerLazySingleton<AbilityStore>(
       () => AbilityStore(
@@ -120,9 +140,9 @@ class Inject {
 
     getIt.registerLazySingleton<PokemonStore>(
       () => PokemonStore(
-        getIt.get<PokemonUseCase>(),
-        CacheFavorites(),
+        getIt<PokemonUseCase>(),
         getIt<MyTeamUseCase>(),
+        getIt<MyFavoritesUseCase>(),
       ),
     );
   }
