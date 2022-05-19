87c42f34a81dafd0437f9034f4bd6c5dd09f44e5
GabLeal <gabriel.leal@compasso.com.br>
2022-05-18 23:02:01 -0300

    transferindo responsabilidade para o datasource

--- lib/layers/data/repositories/move_repository_imp.dart
+++ lib/layers/data/repositories/move_repository_imp.dart
@@ -1,26 +1,22 @@
-import 'package:dio/dio.dart';
-import 'package:pokedex/layers/data/dto/move_details_dto.dart';
+import 'package:either_dart/either.dart';
+import 'package:pokedex/core/failure/datasource_failure.dart';
+import 'package:pokedex/core/failure/failure.dart';
+import 'package:pokedex/layers/data/datasources/remote/moves/move_datasource.dart';
 import 'package:pokedex/layers/domain/entities/move_details_entity.dart';
 import 'package:pokedex/layers/domain/repositories/move_repository.dart';
 
 class MoveDetailsRepositoryImp implements MoveDetailsRepository {
-  final Dio dio;
-  MoveDetailsRepositoryImp({
-    required this.dio,
-  });
+  final MoveDatasource _moveDetailsDatasource;
+  MoveDetailsRepositoryImp(this._moveDetailsDatasource);
 
   @override
-  Future<MoveDetailsEntity?> getMoveDetails(String url) async {
+  Future<Either<Failure, MoveDetailsEntity>> getMoveDetails(String url) async {
     try {
-      var response = await dio.get(url);
+      var result = await _moveDetailsDatasource.getMoveDetails(url);
 
-      var moveDetailsResponse = response.data;
-
-      MoveDetailsDto moveDetails = MoveDetailsDto.fromJson(moveDetailsResponse);
-
-      return moveDetails;
+      return result;
     } catch (erro) {
-      return null;
+      return Left(DatasourceFailure());
     }
   }
 }
