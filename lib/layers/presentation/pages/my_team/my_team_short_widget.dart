4c6ff770c9003219e0022e1997d615d270f1dc80
GabLeal <gabriel.leal@compasso.com.br>
2022-05-21 12:09:32 -0300

    deixando o espaçamento entre as pokebolas com tamanho responsivo

--- lib/layers/presentation/pages/my_team/my_team_short_widget.dart
+++ lib/layers/presentation/pages/my_team/my_team_short_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pokedex/core/extension/list_extension.dart';
 import 'package:pokedex/core/util/constants.dart';
 import 'package:pokedex/layers/presentation/components/cache_image_widget.dart';
 import 'package:pokedex/layers/presentation/stores/pokemon_store.dart';
@@ -32,56 +33,78 @@ class _MyTeamShortWidgetState extends State<MyTeamShortWidget> {
                 padding: const EdgeInsets.only(left: defaultPadding),
                 child: Text('My team'),
               ),
-              Row(
-                children: [
-                  const SizedBox(width: defaultPadding),
-                  Stack(
-                    children: [
-                      Row(
-                        children: List.generate(
-                          6,
-                          (index) => Padding(
-                            padding: const EdgeInsets.only(
-                                right: defaultPadding / 2),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.black,
-                              child: Image.asset(AppImages.whitePokeball),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: List.generate(
-                          _pokemonStore.myTeamPokemon.length,
-                          (index) => Padding(
-                            padding: const EdgeInsets.only(
-                                right: defaultPadding / 2),
-                            child: Hero(
-                              tag: _pokemonStore.myTeamPokemon[index].order!
-                                      .toString() +
-                                  '_cartTag',
-                              child: CircleAvatar(
-                                backgroundColor: Colors.black.withOpacity(0.5),
-                                child: CacheImageWidget(
-                                  pathImage: _pokemonStore.myTeamPokemon[index]
-                                      .sprites!.frontDefault!,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: defaultPadding / 2,
+                    ),
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(
+                              deafultMaxLengthTeamPokemons,
+                              (index) => CircleAvatar(
+                                backgroundColor: Colors.black,
+                                child: Image.asset(
+                                  AppImages.whitePokeball,
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: defaultPadding / 2),
-                    child: Text(
-                      _pokemonStore.myTeamPokemon.length.toString(),
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black87),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(
+                              deafultMaxLengthTeamPokemons,
+                              (index) {
+                                var indexExists =
+                                    _pokemonStore.myTeamPokemon.containsIndex(
+                                  index,
+                                );
+
+                                return indexExists
+                                    ? Hero(
+                                        tag: _pokemonStore
+                                                .myTeamPokemon[index].order!
+                                                .toString() +
+                                            '_cartTag',
+                                        child: CircleAvatar(
+                                          backgroundColor:
+                                              Colors.black.withOpacity(0.5),
+                                          child: CacheImageWidget(
+                                            pathImage: _pokemonStore
+                                                .myTeamPokemon[index]
+                                                .sprites!
+                                                .frontDefault!,
+                                          ),
+                                        ),
+                                      )
+                                    : CircleAvatar(
+                                        backgroundColor: Colors.transparent,
+                                      );
+                              },
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: defaultPadding / 2,
+                      ),
+                      child: Text(
+                        _pokemonStore.myTeamPokemon.length.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           );
