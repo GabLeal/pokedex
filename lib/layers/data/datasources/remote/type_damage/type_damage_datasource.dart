168b0e48bfc6aeedde33e7320632051907ed1385
GabLeal <gabriel.leal@compasso.com.br>
2022-05-21 01:36:56 -0300

    mudando retorno da funcao

--- lib/layers/data/datasources/remote/type_damage/type_damage_datasource.dart
+++ lib/layers/data/datasources/remote/type_damage/type_damage_datasource.dart
@@ -1,7 +1,7 @@
 import 'package:either_dart/either.dart';
 import 'package:pokedex/core/failure/failure.dart';
-import 'package:pokedex/layers/domain/entities/type_damage_entity.dart';
+import 'package:pokedex/layers/data/dto/type_damage_dto.dart';
 
-abstract class TypeDamageDatasource{
-   Future<Either<Failure, TypeDamage>> getTypeDamage(String url);
+abstract class TypeDamageDatasource {
+  Future<Either<Failure, TypeDamage>> getTypeDamage(String url);
 }
