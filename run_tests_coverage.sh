9b7f961be60b9487d5a8c47a7c25d65439c98806
GabLeal <gabriel.leal@compasso.com.br>
2022-05-21 01:26:17 -0300

    removendo arquivo com extensao .g.dart do realtorio do coverage

--- run_tests_coverage.sh
+++ run_tests_coverage.sh
@@ -1,4 +1,4 @@
 flutter test --coverage &&
-lcov --remove coverage/lcov.info 'lib/settings/*' 'lib/presentation/*/view/*' 'lib/presentation/components/*' 'lib/*/components/*' 'lib/*/widgets/*' 'lib/*/widget/*' -o coverage/new_lcov.info &&
+lcov --remove coverage/lcov.info 'lib/layers/presentation/stores/*.g.dart'  -o coverage/new_lcov.info &&
 genhtml coverage/new_lcov.info --output=coverage &&
 open coverage/index.html
