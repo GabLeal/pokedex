fc96bc21ad012f2c7a9c2f13a53890f005f38e91
GabLeal <gabriel.leal@compasso.com.br>
2022-05-22 18:10:26 -0300

    removendo failure nao utilizada

--- lib/core/failure/max_team_failure.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pokedex/core/failure/failure.dart';
-
-class MaxTeamFailure extends Failure {
-  MaxTeamFailure({
-    String message = 'Your team has the maximum number of pokemons',
-  }) : super(message);
-}
